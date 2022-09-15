f34e948f37ef9d53d680a690a9dcec3295493465
Nicole Bracy <contact@nicolebracy.com>
2022-09-15 09:35:51 -0400

    Add logic for sending emails from contact form

--- home/views.py
+++ home/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import EmailMessage
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from .forms import ContactForm
 from .models import Commit
 
 
 def index(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            subj = f'Contact Form: {form.cleaned_data["subj"]}'
+            msg = form.cleaned_data['msg']
+            email_msg = EmailMessage(subj, msg, email, ["contact@nicolebracy.com"], reply_to=[f'{name} <{email}>'])
+            email_msg.send()
+            messages.success(request, 'Email sent, thank you!')
+            return redirect('index')
+
     db_commits = Commit.objects.order_by('-date').all()[:3]
     form = ContactForm()
     context = {
