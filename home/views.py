669554797c448297f69b470414e3e40aee24d501
Nicole Bracy <contact@nicolebracy.com>
2022-09-15 09:22:26 -0400

    Import ContactForm and add to context

--- home/views.py
+++ home/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ContactForm
 from .models import Commit
 
 
 def index(request):
     db_commits = Commit.objects.order_by('-date').all()[:3]
+    form = ContactForm()
     context = {
         'title': 'Home',
-        'commits': db_commits
+        'commits': db_commits,
+        'form': form
     }
     return render(request, 'home/index.html', context)
