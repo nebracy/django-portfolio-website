66bccfede1deaf2bc5037856df1c2db2ac9fc097
Nicole Bracy <contact@nicolebracy.com>
2022-09-16 13:21:32 -0400

    Add all static files for home app & update background-image urls

---
+++ home/static/home/script.js
@@ -0,0 +1,25 @@
+const navDropdown = document.getElementById("dropdown");
+const navHamburger = document.getElementById("hamburger");
+const screenOverlay = document.getElementById("overlay");
+const maxWidth = window.matchMedia("(max-width: 650px)");
+
+
+function checkScreenSize() {
+    if (navDropdown.classList.contains("nav__menu--show")) {
+        navDropdown.classList.remove("nav__menu--show");
+        navHamburger.classList.remove("nav__hamburger--x");
+        screenOverlay.classList.remove("screen-overlay--dim");
+    }
+}
+
+document.addEventListener("click", e => {
+    if (e.target.closest("#dropbtn") && maxWidth.matches) {    // Screen less than 650px
+        navDropdown.classList.toggle("nav__menu--show");
+        navHamburger.classList.toggle("nav__hamburger--x");
+        screenOverlay.classList.toggle("screen-overlay--dim");
+      } else if (maxWidth.matches) {
+            checkScreenSize();
+      }
+});
+
+maxWidth.addEventListener("change", checkScreenSize);
