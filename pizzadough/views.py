149da9d8740b6343d3d848e8eecbeb7f3054cc15
Nicole Bracy <contact@nicolebracy.com>
2022-09-17 15:07:03 -0400

    Add the minimum html for the form & check for POST request and valid form

--- pizzadough/views.py
+++ pizzadough/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import DoughCalculatorForm
 
 
 def calculator(request):
     form = DoughCalculatorForm()
+    if request.method == "POST":
+        form = DoughCalculatorForm(request.POST)
+        if form.is_valid():
+            print('OK')
+            return redirect('pizza-dough')
     context = {
         'form': form
     }
