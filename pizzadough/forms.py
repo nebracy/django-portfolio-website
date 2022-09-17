a61f74d7698a3e28f23c72eb8f071a114181ece9
Nicole Bracy <contact@nicolebracy.com>
2022-09-17 16:48:54 -0400

    Add most of the logic to get the percent, grams and ounces for each ingredient

--- pizzadough/forms.py
+++ pizzadough/forms.py
@@ -6,13 +6,13 @@ OUNCES_GRAMS = [('ounces', 'oz'), ('grams', 'g')]
 
 
 class DoughCalculatorForm(forms.Form):
-    choice = forms.ChoiceField(label='TF/Weight', choices=TH_FACTOR_DOUGH_WT, initial='th_factor', widget=forms.RadioSelect)
+    choice_set = forms.ChoiceField(label='TF/Weight', choices=TH_FACTOR_DOUGH_WT, initial='th_factor', widget=forms.RadioSelect)
     flour = forms.IntegerField(min_value=100, max_value=100, initial=100, widget=forms.HiddenInput)
-    dough_wt = forms.DecimalField(label='Dough Weight', min_value=1, max_value=20000)
-    g_oz = forms.ChoiceField(choices=OUNCES_GRAMS, initial='grams', widget=forms.RadioSelect)
-    thk_factor = forms.DecimalField(label='Thickness Factor', widget=forms.NumberInput(attrs={'type': 'range', 'min': '0.07', 'max': '0.1', 'value': '0.09', 'step': '0.005)'}))
-    pizza_size = forms.IntegerField(label='Pizza Size (in)', min_value=12, max_value=22, initial=16)
-    pizza_num = forms.IntegerField(label='Pizza(s)', min_value=1, max_value=25, initial=1)
+    dough_wt_set = forms.DecimalField(label='Dough Weight', min_value=1, max_value=20000)
+    g_oz_set = forms.ChoiceField(choices=OUNCES_GRAMS, initial='grams', widget=forms.RadioSelect)
+    thk_factor_set = forms.DecimalField(label='Thickness Factor', widget=forms.NumberInput(attrs={'type': 'range', 'min': '0.07', 'max': '0.1', 'value': '0.09', 'step': '0.005)'}))
+    pizza_size_set = forms.IntegerField(label='Pizza Size (in)', min_value=12, max_value=22, initial=16)
+    pizza_num_set = forms.IntegerField(label='Pizza(s)', min_value=1, max_value=25, initial=1)
     water = forms.IntegerField(label='Hydration', widget=forms.NumberInput(attrs={'type': 'range', 'min': '55', 'max': '70', 'value': '61'}))
     yeast = forms.DecimalField(label='Yeast %', min_value=0, max_value=3, step_size=0.05, initial=0.5)
     salt = forms.DecimalField(label='Salt %', min_value=0, max_value=4, step_size=0.05, initial=1.5)
--- pizzadough/views.py
+++ pizzadough/views.py
@@ -1,3 +1,5 @@
+import math
+from decimal import Decimal
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from .forms import DoughCalculatorForm
@@ -8,7 +10,28 @@ def calculator(request):
     if request.method == "POST":
         form = DoughCalculatorForm(request.POST)
         if form.is_valid():
-            print('OK')
+            data = form.cleaned_data
+            dough = {ing: {'Percent': data[ing]} for ing in data if not ing.endswith('_set')}
+
+            if data['dough_wt_set'] and data['choice_set'] == 'dough_wt':
+                dough_wt = data['dough_wt_set']
+            else:
+                radius2 = (data['pizza_size_set'] / 2) ** 2
+                dough_wt = data['thk_factor_set'] * Decimal(math.pi * radius2)
+
+            total_pct = sum(val['Percent'] for val in dough.values())
+            flour_wt = dough_wt * data['pizza_num_set'] / (total_pct / 100)
+
+            dough |= {'total': {'Percent': total_pct}}
+            for kv in dough.values():
+                weight = flour_wt * (kv['Percent']) / 100
+                kv['Grams'] = kv['Ounces'] = weight
+                if data['g_oz_set'] == 'g' and data['choice'] == 'Dough Weight':
+                    kv |= {'Ounces': weight * Decimal(0.03527396195)}
+                else:
+                    kv |= {'Grams': weight * Decimal(28.349523125)}
+
+            print(dough)
             return redirect('pizza-dough')
     context = {
         'form': form
