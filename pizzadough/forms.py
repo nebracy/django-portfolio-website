fa537815f65c2f3cbd24ad0045a10844e4a224f2
Nicole Bracy <contact@nicolebracy.com>
2022-09-19 13:54:50 -0400

    Add clean method to DoughCalculatorForm class for extra validation & for dough_wt_set and pizza_size_set add required=False

--- pizzadough/forms.py
+++ pizzadough/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 TH_FACTOR_DOUGH_WT = [('dough_wt', 'Dough Weight'), ('th_factor', 'Thickness Factor')]
@@ -8,10 +9,10 @@ OUNCES_GRAMS = [('ounces', 'oz'), ('grams', 'g')]
 class DoughCalculatorForm(forms.Form):
     choice_set = forms.ChoiceField(label='TF/Weight', choices=TH_FACTOR_DOUGH_WT, initial='th_factor', widget=forms.RadioSelect)
     flour = forms.IntegerField(min_value=100, max_value=100, initial=100, widget=forms.HiddenInput)
-    dough_wt_set = forms.DecimalField(label='Dough Weight', min_value=1, max_value=20000)
+    dough_wt_set = forms.DecimalField(label='Dough Weight', min_value=1, max_value=20000, required=False)
     g_oz_set = forms.ChoiceField(choices=OUNCES_GRAMS, initial='grams', widget=forms.RadioSelect)
     thk_factor_set = forms.DecimalField(label='Thickness Factor', widget=forms.NumberInput(attrs={'type': 'range', 'min': '0.07', 'max': '0.1', 'value': '0.09', 'step': '0.005'}))
-    pizza_size_set = forms.IntegerField(label='Pizza Size (in)', min_value=12, max_value=22, initial=16)
+    pizza_size_set = forms.IntegerField(label='Pizza Size (in)', min_value=12, max_value=22, initial=16, required=False)
     pizza_num_set = forms.IntegerField(label='Pizza(s)', min_value=1, max_value=25, initial=1)
     water = forms.IntegerField(label='Hydration', widget=forms.NumberInput(attrs={'type': 'range', 'min': '55', 'max': '70', 'value': '61'}))
     yeast = forms.DecimalField(label='Yeast %', min_value=0, max_value=3, step_size=0.05, initial=0.5)
@@ -19,3 +20,11 @@ class DoughCalculatorForm(forms.Form):
     olive_oil = forms.DecimalField(label='Olive Oil %', min_value=0, max_value=8, step_size=0.05, initial=3, required=False)
     sugar = forms.DecimalField(label='Sugar %', min_value=0, max_value=4, step_size=0.05, initial=1.5, required=False)
     # opt = forms.DecimalField(label='Ingredient', min_value=0, max_value=50, step_size=0.05, initial=0, required=False)
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if cleaned_data.get("choice_set") == 'dough_wt' and not cleaned_data.get("dough_wt_set"):
+            raise ValidationError('Dough Weight is required.')
+        elif cleaned_data.get("choice_set") == 'th_factor' and not cleaned_data.get("pizza_size_set"):
+            raise ValidationError('Pizza Size is required.')
