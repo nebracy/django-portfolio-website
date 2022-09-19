c887799d49387c9a38a03ca9c3f20305505fbe36
Nicole Bracy <contact@nicolebracy.com>
2022-09-18 23:41:25 -0400

    Move POST logic from calculator to calculate view

--- pizzadough/views.py
+++ pizzadough/views.py
@@ -2,44 +2,42 @@ import math
 from decimal import Decimal
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.views.decorators.http import require_GET, require_POST
 from .forms import DoughCalculatorForm
 
 
+@require_GET
 def calculator(request):
     form = DoughCalculatorForm()
-    dough = None
-    if request.method == "POST":
-        form = DoughCalculatorForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            dough = {ing.replace('_', ' '): {'Percent': data[ing]} for ing in data if not ing.endswith('_set')}
-
-            if data['dough_wt_set'] and data['choice_set'] == 'dough_wt':
-                dough_wt = data['dough_wt_set']
-            else:
-                radius2 = (data['pizza_size_set'] / 2) ** 2
-                dough_wt = data['thk_factor_set'] * Decimal(math.pi * radius2)
-
-            total_pct = sum(val['Percent'] for val in dough.values())
-            flour_wt = dough_wt * data['pizza_num_set'] / (total_pct / 100)
-
-            dough |= {'total': {'Percent': total_pct}}
-            for kv in dough.values():
-                weight = flour_wt * (kv['Percent']) / 100
-                kv['Grams'] = kv['Ounces'] = weight
-                if data['g_oz_set'] == 'g' and data['choice'] == 'Dough Weight':
-                    kv |= {'Ounces': weight * Decimal(0.03527396195)}
-                else:
-                    kv |= {'Grams': weight * Decimal(28.349523125)}
-
-            print(dough)
-            # return redirect('pizza-dough')
     context = {
-        'form': form,
-        'dough': dough
+        'form': form
     }
     return render(request, 'pizzadough/calculator.html', context)
 
 
+@require_POST
 def calculate(request):
-    return render(request, 'pizzadough/calculate.html')
+    form = DoughCalculatorForm(request.POST)
+    dough = None
+    if form.is_valid():
+        data = form.cleaned_data
+        dough = {ing.replace('_', ' '): {'Percent': data[ing]} for ing in data if not ing.endswith('_set')}
+
+        if data['dough_wt_set'] and data['choice_set'] == 'dough_wt':
+            dough_wt = data['dough_wt_set']
+        else:
+            radius2 = (data['pizza_size_set'] / 2) ** 2
+            dough_wt = data['thk_factor_set'] * Decimal(math.pi * radius2)
+
+        total_pct = sum(val['Percent'] for val in dough.values())
+        flour_wt = dough_wt * data['pizza_num_set'] / (total_pct / 100)
+
+        dough |= {'total': {'Percent': total_pct}}
+        for kv in dough.values():
+            weight = flour_wt * (kv['Percent']) / 100
+            kv['Grams'] = kv['Ounces'] = weight
+            if data['g_oz_set'] == 'g' and data['choice'] == 'Dough Weight':
+                kv |= {'Ounces': weight * Decimal(0.03527396195)}
+            else:
+                kv |= {'Grams': weight * Decimal(28.349523125)}
+    return render(request, 'pizzadough/calculate.html', {'dough': dough})
