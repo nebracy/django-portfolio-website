e851eebaa9bac149d1fcfc90896a17b68d116014
Nicole Bracy <contact@nicolebracy.com>
2022-09-19 02:07:02 -0400

    Return 204 if form is not valid and refactor calculate view

--- pizzadough/views.py
+++ pizzadough/views.py
@@ -17,26 +17,28 @@ def calculator(request):
 @require_POST
 def calculate(request):
     form = DoughCalculatorForm(request.POST)
-    recipe_table = None
-    if form.is_valid():
-        data = form.cleaned_data
-        recipe_table = {ing.replace('_', ' '): {'Percent': data[ing]} for ing in data if not ing.endswith('_set')}
 
-        if data['dough_wt_set'] and data['choice_set'] == 'dough_wt':
-            dough_wt = data['dough_wt_set']
+    if not form.is_valid():
+        return HttpResponse(status=204)
+
+    data = form.cleaned_data
+    recipe_table = {ing.replace('_', ' '): {'Percent': data[ing]} for ing in data if not ing.endswith('_set')}
+
+    if data['dough_wt_set'] and data['choice_set'] == 'dough_wt':
+        dough_wt = data['dough_wt_set']
+    else:
+        radius2 = (data['pizza_size_set'] / 2) ** 2
+        dough_wt = data['thk_factor_set'] * Decimal(math.pi * radius2)
+
+    total_pct = sum(val['Percent'] for val in recipe_table.values())
+    flour_wt = dough_wt * data['pizza_num_set'] / (total_pct / 100)
+
+    recipe_table |= {'total': {'Percent': total_pct}}
+    for kv in recipe_table.values():
+        weight = flour_wt * (kv['Percent']) / 100
+        kv['Grams'] = kv['Ounces'] = weight
+        if data['g_oz_set'] == 'g' and data['choice'] == 'Dough Weight':
+            kv |= {'Ounces': weight * Decimal(0.03527396195)}
         else:
-            radius2 = (data['pizza_size_set'] / 2) ** 2
-            dough_wt = data['thk_factor_set'] * Decimal(math.pi * radius2)
-
-        total_pct = sum(val['Percent'] for val in recipe_table.values())
-        flour_wt = dough_wt * data['pizza_num_set'] / (total_pct / 100)
-
-        recipe_table |= {'total': {'Percent': total_pct}}
-        for kv in recipe_table.values():
-            weight = flour_wt * (kv['Percent']) / 100
-            kv['Grams'] = kv['Ounces'] = weight
-            if data['g_oz_set'] == 'g' and data['choice'] == 'Dough Weight':
-                kv |= {'Ounces': weight * Decimal(0.03527396195)}
-            else:
-                kv |= {'Grams': weight * Decimal(28.349523125)}
+            kv |= {'Grams': weight * Decimal(28.349523125)}
     return render(request, 'pizzadough/table.html', {'recipe_table': recipe_table})
