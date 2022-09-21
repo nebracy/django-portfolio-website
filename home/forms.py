9c6f9335e4dd519ce9845aeff11c797f990b8ca7
Nicole Bracy <contact@nicolebracy.com>
2022-09-21 18:39:30 -0400

    Add placeholder text to contact form fields

--- home/forms.py
+++ home/forms.py
@@ -4,10 +4,10 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    subj = forms.CharField(label='Subject', max_length=100)
-    msg = forms.CharField(label='Message', widget=forms.Textarea)
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Name'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    subj = forms.CharField(label='Subject', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Subject'}))
+    msg = forms.CharField(label='Message', widget=forms.Textarea(attrs={'placeholder': 'Message'}))
 
     def send_email(self):
         email_msg = EmailMessage(
