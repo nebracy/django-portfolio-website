091fa67bd8767bb59ebc9adf5fbe3b003a7eb4d3
Nicole Bracy <contact@nicolebracy.com>
2022-09-30 10:08:13 -0400

    Create initial functions

--- home/migrations/0002_load_initial_data.py
+++ home/migrations/0002_load_initial_data.py
@@ -3,6 +3,15 @@
 from django.db import migrations
 
 
+def load_initial_data(apps, schema_editor):
+    pass
+
+
+def delete_all_data(apps, schema_editor):
+    Commit = apps.get_models('home', 'Commit')
+    Commit.objects.all().delete()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -10,4 +19,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(load_initial_data, delete_all_data)
     ]
